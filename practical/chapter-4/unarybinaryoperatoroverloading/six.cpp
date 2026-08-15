42455d8bc716de7da69c82c0f731f20403595288
the-bsx <bishalbhusal005@gmail.com>
2026-08-15 12:22:10 +0545

    unarybinary operator overloading

--- practical/chapter-4/unarybinaryoperatoroverloading/six.cpp
+++ practical/chapter-4/unarybinaryoperatoroverloading/six.cpp
@@ -6,10 +6,35 @@
 
 
 #include<iostream>
+#include<string>
+#include<cstring>
 using namespace std;
 
 
 class MyString {
     private:
         char str[100];
+    public:
+        MyString(const char* s = ""){
+            strcpy(str, s);
+        }
+        MyString operator+(const MyString& s) {
+            MyString temp;
+            strcpy(temp.str, str);
+            strcat(temp.str, s.str);
+            return temp;
+        }
+        void display() const {
+            cout << str<< endl;
+        }
 };
+
+int main() {
+    MyString s1("Hello ");
+    MyString s2("World!");
+    MyString s3 = s1 + s2;
+
+    s3.display(); // expected output: Hello world!
+
+    return 0;
+}
